7be2bc75c3b2e419fc436480e43794347ed213e9
Madde Lundström <madde.lundstrom@edu.edugrade.se>
2022-10-24 16:11:29 +0200

    New edit 22.10.24

--- 22_10_20_Countries/Program.cs
+++ 22_10_20_Countries/Program.cs
@@ -13,29 +13,66 @@ namespace _22_10_20_Countries
             Country C3 = new Country("Norge", "Nor", "Oslo");
             Country C4 = new Country("Syrien", "Syr", "Damascus");
             Country C5 = new Country("Algeriet", "DZA", "Alger");
-            
+
             List<Country> countries = new List<Country>()
             {
                 C1, C2, C3, C4, C5
             };
 
-            string cont = "Y";
-            while (cont != "N")
+            CountryCodesMethod();
+
+            void CountryCodesMethod()
             {
-                Console.WriteLine("Write a country code: ");
-                string input = Console.ReadLine().ToUpper();
-                foreach (var country in countries)
+                bool run = true;
+                while (run)
                 {
-                    Console.Clear();
-                    if (input == country.Code)
+                    Console.WriteLine("Write a country code: ");
+                    string input = Console.ReadLine().ToUpper();
+
+                    foreach (var country in countries)
                     {
-                        Console.WriteLine(country.Name + " " + country.Capital);
-                        break;
+                        Console.Clear();
+                        if (input == country.Code)
+                        {
+                            Console.WriteLine(country.Name + " " + country.Capital);
+                            break;
+                        }
+                        Console.WriteLine("Country code not valid!");
                     }
-                    Console.WriteLine("Country code not valid!");
+                    TryAgain();
                 }
-                Console.WriteLine("Do you wanna try again? Y/N");
-                cont = Console.ReadLine().ToUpper();
+            }
+            void TryAgain()
+            {
+                bool runCheck = true;
+                while (runCheck)
+                {
+                    Console.WriteLine("Do you want to check another country code? Y/N");
+                    string cont = Console.ReadLine().ToUpper();
+                    if (string.IsNullOrWhiteSpace(cont) == false)
+                    {
+                        switch (cont)
+                        {
+                            case "Y":
+                                CountryCodesMethod();
+                                Console.Clear();
+                                break;
+                            case "N":
+                                Environment.Exit(0);
+                                break;
+                            default:
+                                Console.Clear();
+                                Console.WriteLine("Invalid choice. Try again.");
+                                TryAgain();
+                                break;
+                        }
+                    }
+                    else if (string.IsNullOrWhiteSpace(cont) == true)
+                    {
+                        Console.Clear();
+                        Console.WriteLine("Invalid choice. Try again.");
+                    }
+                }
             }
         }
     }
