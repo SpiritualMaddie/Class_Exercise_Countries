5fde47e0821d33d9c83a25245006b0210da587f9
Madde Lundström <madde.lundstrom@edu.edugrade.se>
2022-10-24 11:36:09 +0200

    Add project files.

---
+++ 22_10_20_Countries/Country.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace _22_10_20_Countries
+{
+    class Country
+    {
+        public string Name { get; set; }
+        public string Code { get; set; }
+        public string Capital { get; set; }
+        public Country(string Name, string Code, string Capital)
+        {
+            this.Name = Name;
+            this.Code = Code.ToUpper();
+            this.Capital = Capital;
+        }
+    }
+}
---
+++ 22_10_20_Countries/Program.cs
@@ -0,0 +1,42 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+
+namespace _22_10_20_Countries
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Country C1 = new Country("Sweden", "swe", "Stockholm");
+            Country C2 = new Country("Canada", "can", "Ottawa");
+            Country C3 = new Country("Norge", "Nor", "Oslo");
+            Country C4 = new Country("Syrien", "Syr", "Damascus");
+            Country C5 = new Country("Algeriet", "DZA", "Alger");
+            
+            List<Country> countries = new List<Country>()
+            {
+                C1, C2, C3, C4, C5
+            };
+
+            string cont = "Y";
+            while (cont != "N")
+            {
+                Console.WriteLine("Write a country code: ");
+                string input = Console.ReadLine().ToUpper();
+                foreach (var country in countries)
+                {
+                    Console.Clear();
+                    if (input == country.Code)
+                    {
+                        Console.WriteLine(country.Name + " " + country.Capital);
+                        break;
+                    }
+                    Console.WriteLine("Country code not valid!");
+                }
+                Console.WriteLine("Do you wanna try again? Y/N");
+                cont = Console.ReadLine().ToUpper();
+            }
+        }
+    }
+}
